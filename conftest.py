b3855efb8c4195c1741f07b1198da2160a7f63b2
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-01-23 11:00:40 +0000

    trying pytest fixtures

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+# conftest allows fixture functions to be used in multiple test files
+@pytest.fixture
+def input_value():
+    input = 39
+    return input
+
+# this fixture returns a function which generates data
+@pytest.fixture
+def make_customer_record():
+    def _make_customer_record(name):
+        return {"name": name, "orders": []}
+
+    return _make_customer_record
+
+# tries the test with both parameters - to see if the test passes with both
+@pytest.fixture(scope='module', params=['purple', 'pink'])
+def try_parametrisation(request):
+    colour = request.param
+    return colour
---
+++ test_pytest_fixtures.py
@@ -0,0 +1,22 @@
+# example pytest fixture taken from the internet
+
+def test_divisible_by_3(input_value):
+    assert input_value % 3 == 0
+
+
+def test_divisible_by_6(input_value):
+    try:
+        assert input_value % 6 == 0
+    except AssertionError as err:
+        pass
+
+
+def test_customer_records(make_customer_record):
+    customer_1 = make_customer_record("Lisa")
+    customer_2 = make_customer_record("Mike")
+    assert customer_1['name'] == 'Lisa'
+    assert customer_2['name'] == 'Mike'
+
+
+def test_parametrisation(try_parametrisation):
+    assert try_parametrisation == 'purple'
