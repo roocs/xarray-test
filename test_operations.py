6d108a5891a5c9ae669ab43256d87c63cb56a18c
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-01-23 10:59:25 +0000

    trying pytest fixtures

--- test_operations.py
+++ test_operations.py
@@ -1,4 +1,6 @@
 import xarray as xr
+import numpy as np
+# import xesmf as xe
 
 fpath = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/atmos/Amon/r1i1p1/latest' \
         '/tas/tas_Amon_HadGEM2-ES_historical_r1i1p1_193412-195911.nc'
@@ -50,7 +52,7 @@ def test_open_multiple_variable_files():
     try:
         ds = xr.open_mfdataset(all_vars)
         return ds
-    except MergeError as ex:
+    except xr.MergeError as ex:
         pass
 
 
@@ -110,6 +112,26 @@ def test_avg_subset_along_time():
 def test_max_of_a_time_slice():
     ds = xr.open_mfdataset(files)
     time_slice = ds.sel(time=slice('1922-01-16', '1931-12-16'))
-    max = time_slice.tas.max(dim='time')
-    assert max.shape == (145, 192)
+    maximum = time_slice.tas.max(dim='time')
+    assert maximum.shape == (145, 192)
 
+
+def test_mean_of_lat_on_time_slice():
+    ds = xr.open_mfdataset(files)
+    time_slice = ds.sel(time=slice('1922-01-16', '1931-12-16'))
+    mean = time_slice.tas.mean(dim='lat')
+    assert mean.shape == (120, 192)
+
+
+# def test_regridding():
+#     ds = xr.open_mfdataset('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/atmos/Amon/r1i1p1/latest/ta/*.nc')
+#
+#     ds_out = xr.Dataset({'lat': (['lat'], np.arange(16, 75, 1.0)),
+#                          'lon': (['lon'], np.arange(200, 330, 1.5)),
+#                          }
+#                         )
+#
+#     regridder = xe.Regridder(ds, ds_out, 'bilinear')
+#     regridder.clean_weight_file()
+#     ds_out = regridder(ds)
+#     print(ds_out)
